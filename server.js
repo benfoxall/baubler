94865a8e45c0cfe2a124de4b1f3146f056ef130e
Ben Foxall <benfoxall@gmail.com>
2014-01-19 12:16:56 +0000

    multi-process with redis pubsub

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var WebSocketServer = require('ws').Server
   , http = require('http')
   , express = require('express')
   , app = express()
-  , port = process.env.PORT || 5000;
+  , port = process.env.PORT || 5000
+  , redis_sub = require('redis-url').connect(process.env.REDISTOGO_URL)
+  , redis_pub = require('redis-url').connect(process.env.REDISTOGO_URL);
+
 
 app.use(express.static(__dirname + '/'));
 
@@ -15,13 +18,7 @@ console.log('http server listening on %d', port);
 var wss = new WebSocketServer({server: server});
 console.log('websocket server created');
 wss.on('connection', function(ws) {
-    // var id = setInterval(function() {
-    //     // ws.send(JSON.stringify(new Date()), function() {  });
-
-    //     var msg = [0,0,0,0].map(function(){return Math.floor(Math.random()*500)});
-    //     ws.send(msg.join(','));
-    // }, 10);
-
+  
     console.log('websocket connection open');
 
     ws.on('close', function() {
@@ -30,19 +27,20 @@ wss.on('connection', function(ws) {
     });
 
     ws.on('message', function(data){
-      // todo - don't echo back here
-      for(var i in wss.clients)
-        wss.clients[i].send(data);
+      // todo - check format of message
+      redis_pub.publish('draws', data)
     })
 
 
 
 });
 
+redis_sub.on("message", function (channel, message) {
+    console.log("client1 channel " + channel + ": ", message);
+    // send out to all clients on this node
+    // todo - don't echo to emitting node
+    for(var i in wss.clients)
+        wss.clients[i].send(message);
+});
 
-
-
-// wss.broadcast = function(data) {
-//     for(var i in this.clients)
-//         this.clients[i].send(data);
-// };
+redis_sub.subscribe("draws");
