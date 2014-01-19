42e8e2184eddec69fdf7bdc891ec247ebae4b451
Ben Foxall <benfoxall@gmail.com>
2014-01-19 13:34:30 +0000

    persist points

--- js/main.js
+++ js/main.js
@@ -34,6 +34,7 @@ capture.onclick = function(){
 
   var prior_x, prior_y;
 
+  var drawstate = 'none', button_size = 50, points = [];
 
   window.b = new Bauble({worker:'js/bauble-worker.js'})
   b.getUserMedia()
@@ -51,17 +52,52 @@ capture.onclick = function(){
 
       // strokeRect(x-3,y-3, 6,6)
 
+      // if we have start and end points
       if(x && y && prior_x && prior_y){
         beginPath();
         moveTo(prior_x, prior_y);
         lineTo(x,y);
         stroke();
+
+        // submit to live thing (remove if scale problems)
+        var send_data = [x,y,prior_x,prior_y].map(function(d){return parseInt(d)}).join(',')
+        ws.send(send_data);
+
+        if(drawstate == 'started'){
+          points.push(parseInt(x,10));
+          points.push(parseInt(y,10));
+        }
       }
-      
-      var send_data = [x,y,prior_x,prior_y].map(function(d){return parseInt(d)}).join(',')
-      ws.send(send_data);
                   
     }
+
+
+
+    // update draw states
+    if(x && y){
+      if(x < button_size && y > b.canvas.height - button_size){
+        clear();
+        drawstate = 'started';
+        console.log(drawstate)
+      }
+      if(x > b.canvas.width - button_size && y > b.canvas.height - button_size){
+        clear();
+        // submit points
+        if(drawstate === 'started'){
+          if(points.length) 
+            reqwest({
+              method:'post',
+              url:'/', 
+              data:{data:points}, type:'json'
+            })
+          points = [];
+        }
+
+        drawstate = 'none';
+        console.log(drawstate)
+      }
+    }
+
     prior_x = x, prior_y = y;
   });
 
@@ -70,6 +106,15 @@ capture.onclick = function(){
     b.pctx.clearRect(0,0,b.canvas.width,b.canvas.height);
     b.pctx.fillStyle = 'rgba(255,255,255,0.7)';
     b.pctx.fillRect(0,0,b.canvas.width,b.canvas.height);
+
+
+    b.pctx.fillStyle = '#08f';
+    b.pctx.fillRect(0,b.canvas.height - button_size
+      ,button_size,button_size);
+
+    b.pctx.fillStyle = '#f08';
+    b.pctx.fillRect(b.canvas.width - button_size,b.canvas.height - button_size
+      ,button_size,button_size);
   }
 
 
